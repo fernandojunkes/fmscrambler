4ee641e9a88f7f6aa829dc885ab537b883794d1f
Fernando Rafael Junkes <fe.junkes@gmail.com>
2023-01-12 19:26:24 -0300

    Created copy project with .Net 6

---
+++ FMEditor/Utility/Meta.cs
@@ -0,0 +1,25 @@
+﻿namespace FMTools.Utility
+{
+    /// <summary>  
+    ///  Meta Information about the Library
+    /// </summary>  
+    public class Meta
+    {
+        /// <summary>  
+        ///  Major Version Identifier as Integer  
+        /// </summary>  
+        public static int MajorVersion = 0;
+        /// <summary>
+        /// Minor Version Identifier as Integer
+        /// </summary>
+        public static int MinorVersion = 1;
+        /// <summary>
+        /// Patch Version Identifier as Integer
+        /// </summary>
+        public static int PatchVersion = 7;
+        /// <summary>
+        /// Extra Version Info String
+        /// </summary>
+        public static string VersionInfo = "Alpha";
+    }
+}
---
+++ FMEditor/Utility/Options.cs
@@ -0,0 +1,88 @@
+﻿using FMTools.Models;
+
+namespace FMTools.Utility
+{
+    public class Options
+    {
+        /// <summary>
+        /// Options (true / false)
+        /// </summary>
+        public bool RandomFusions { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool GlitchFusions { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomAttributes { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomGuardianStars { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomCardDrops { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomTypes { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomStarchips { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool GlitchGuardianStars { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomDecks { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool GlitchAttributes { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomEquips { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool HighId { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool RandomAtkdef { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Spoiler { get; set; } = true;
+
+        /// <summary>
+        /// Count of Cards as Integer
+        /// </summary>
+        public int CardCount { get; set; } = 722;
+
+        /// <summary>
+        /// Path to the Game Folder
+        /// </summary>
+        public string GameBinPath { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string SlusPath { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string WaPath { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string IsoPath { get; set; } = string.Empty;
+
+        public string RandomizerFileName { get; set; } = string.Empty;
+    }
+}
